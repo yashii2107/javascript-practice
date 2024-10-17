3c71252c90492b4f14990b5c115d966314df258c
yashii2107 <yashii2107@gmail.com>
2024-10-17 11:26:02 +0530

    js practice task 1

---
+++ js-practice.js
@@ -0,0 +1,16 @@
+// alert("hello");
+// alert("world");
+
+/**
+ * variables
+ * using let and var we can define variables
+ * let a = 5;
+ * let a =6; ==> error 
+ * 
+ * let stringName = "hello";
+ * const used to define constant values
+ * const ab = 1;
+ * ab=2;==> error;
+ */
+
+
